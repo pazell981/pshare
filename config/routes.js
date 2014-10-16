1d1f678533190fa0411fa7f8c340c59312b83ffd
Paul Zellmer <paulzellmer9@gmail.com>
2014-10-16 14:11:06 -0700

    check-in

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+exports.desktop = function (req, res){
+  res.render('index', { 
+  	title: 'Remember When We...',
+  	event: '',
+  	image1: './images/wedding-party.jpg',
+  	image1Alt: 'Wedding Party',
+  	image1Caption: 'Weddings',
+  	image2: './images/corporate-event.jpg',
+  	image2Alt: 'Corporate Events',
+  	image2Caption: 'Corporate Events',
+  	image3: './images/special-event.jpg',
+  	image3Alt: 'Special Events',
+  	image3Caption: 'Special Events'
+  });
+};
 
-module.exports = router;
+exports.mobile = function (req, res){
+  res.render('mobile', { 
+  	title: 'Remember When We...',
+  	event: '',
+  	image1: './images/wedding-party.jpg',
+  	image1Alt: 'Wedding Party',
+  	image1Caption: 'Weddings',
+  	image2: './images/corporate-event.jpg',
+  	image2Alt: 'Corporate Events',
+  	image2Caption: 'Corporate Events',
+  	image3: './images/special-event.jpg',
+  	image3Alt: 'Special Events',
+  	image3Caption: 'Special Events'
+  });
+};
