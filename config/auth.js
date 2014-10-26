10e1946ba317942d6e4ab6ab04e7314b08b1f41e
Paul Zellmer <paulzellmer9@gmail.com>
2014-10-26 11:08:11 -0700

    aws fixes

--- config/auth.js
+++ config/auth.js
@@ -1,21 +1,20 @@
-var social = require("../config/social.json");
 module.exports = {
 
 	'facebookAuth' : {
 		'clientID' 		: '475092382630434',
-		'clientSecret' 	: process.env.FACEBOOK || social.FACEBOOK,
+		'clientSecret' 	: process.env.FACEBOOK,
 		'callbackURL' 	: 'http://rememberwhenwe.elasticbeanstalk.com/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'kkigdCeqSFJjQZi6mgvJDdVZ3',
-		'consumerSecret' 	: process.env.TWITTER || social.TWITTER,
+		'consumerSecret' 	: process.env.TWITTER,
 		'callbackURL' 		: 'http://rememberwhenwe.elasticbeanstalk.com/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '609171465478-vrbbvlrcl7u4h00atnc8ufg81vpkghtc.apps.googleusercontent.com',
-		'clientSecret' 	: process.env.GOOGLE || social.GOOGLE,
+		'clientSecret' 	: process.env.GOOGLE,
 		'callbackURL' 	: 'http://rememberwhenwe.elasticbeanstalk.com/auth/google/callback'
 	}
 
